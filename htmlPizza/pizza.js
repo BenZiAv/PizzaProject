31aa886657e6221a7da4a22b428d2464979251ce
Gorienich <107270919+Gorienich@users.noreply.github.com>
2022-06-25 22:57:37 +0300

    hash cripto for sesion

--- htmlPizza/pizza.js
+++ htmlPizza/pizza.js
@@ -1,3 +1,12 @@
+const cripto = require('cripto'); /*create key*/
+const fs = require('fs');         /* save file for jsone */
+
+const key = cripto.randomBytes(16).toString('hex' ); /*generate key 16 bites last requier more than 16 bites than toString hexodecimaly */
+const algorithm = 'aes256';  /* type  of algorithm note.js */
+
+fs.writeFile('${__dirname}/key.jsone', JSONE.stringify({key,algorithm}));  /* create jsone key with algorithm */ 
+
+
 function addToPizza(){
     MouseEvent
 }
