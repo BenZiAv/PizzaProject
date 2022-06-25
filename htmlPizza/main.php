c67ff27b879b6af6c57bdfcb42c4d325c4592967
RossEcho <ros303ros@gmail.com>
2022-06-25 22:19:23 +0300

    Update and rename main.html to main.php
    include header

--- htmlPizza/main.php
+++ htmlPizza/main.php
@@ -1,17 +1,4 @@
-<php
-
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/htmlPizza/style.css">
-    <script src="https://kit.fontawesome.com/040f4ea875.js" crossorigin="anonymous"></script>
-    <title>pizzaExample</title>  
-</head>
+<?php include '../src/inc/header.php'; ?>
 <body>
     <header id="header">
         <div>
