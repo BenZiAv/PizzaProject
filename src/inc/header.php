5d85b460d5bb943cfa11589377037f26f2160924
BenZiAv <benavnilov@gmail.com>
2022-06-27 12:24:04 +0300

    style in header.php

--- src/inc/header.php
+++ src/inc/header.php
@@ -4,9 +4,9 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="header.css">
-		<a href="/htmlPizza/style.css"></a>
-        <title>Pizza</title>
+        <link rel="stylesheet" href="/src/inc/header.css">
+		<link rel="stylesheet" href="/htmlPizza/style.css">	
+	<title>Pizza</title>
     </head>
 	<style>
 	</style>
