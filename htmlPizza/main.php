2612c81e3bc1c5460952f4756bab6404073e34bd
BenZiAv <benavnilov@gmail.com>
2022-06-30 00:08:00 +0300

    style.css attached and works  needs to move navbar to the right and to larger fonts

--- htmlPizza/main.php
+++ htmlPizza/main.php
@@ -1,17 +1,10 @@
+<?php include './header.php'; ?>
+
 <body>
     <header id="header">
-        <div>
-            <a href="/pizzaBackend/userForm.php"><i>Profile</i></a>
-        </div>
-        <div>
-            <a href="https://www.independent.co.uk/topic/pizza"><i>news of pizza</i></a>
-        </div>
-        <span><img class="logo" src="/res/logo1.jpg" alt=""></span>      
+        <span><img class="logo" src="../res/logo1.jpg" alt=""></span>      
         <div>
            <a href="#deals"><i>Our deals</i></a>
-        </div>
-        <div>
-            <a href="/pizzaBackend/contactPizza.php"><i>Contact Us</i></a>
         </div>
         <div>
             <a href="#deals"><i>Hot deals</i></a>
@@ -32,25 +25,25 @@
             <div class="deal-conteiner">
                 <a href="#popupDeal">
                 <h2>Family</h2>
-                <div><img class="deal-foto"  src="/res/deal-familyjpg.jpg" alt="family"></div>
+                <div><img class="deal-foto"  src="../res/deal-familyjpg.jpg" alt="family"></div>
                 <p>120ש"ח<br>two large pizza with cola + 2 Additives</p></a>
             </div>
             <div class="deal-conteiner">
                 <a href="#popupDeal">
                 <h2>Big Israel</h2>
-                <div><img class="deal-foto" src="/res/deal1.jpg" alt="bigIsrael"></div>
+                <div><img class="deal-foto" src="../res/deal1.jpg" alt="bigIsrael"></div>
                 <p>180ש"ח<br>two Huge pizza with 2 cola + 4 Additives</p></a>
             </div>
             <div class="deal-conteiner">
                 <a href="popupDeal">
                 <h2>personal</h2>
-                <div><img class="deal-foto" src="/res/isheet.jpg" alt="isheet"></div>
+                <div><img class="deal-foto" src="../res/isheet.jpg" alt="isheet"></div>
                 <p>45ש"ח<br>one small pizza with cola 0.5l + 1 Additive</p></a>
             </div>
             <div class="deal-conteiner">
                 <a href="popupDeal">
                 <h2>corparation Deal</h2>
-                <div><img class="deal-foto" src="/res/corparative.jpg" alt="corporative"></div>
+                <div><img class="deal-foto" src="../res/corparative.jpg" alt="corporative"></div>
                 <p>200ש"ח<br>tree large pizza with cola + 6 Additives</p></a>
             </div>
         </div>
@@ -59,21 +52,21 @@
             <div class="deal-conteiner">
                 <a href="popupPizza">
                 <h2>Huge</h2>
-                <div><img class="pizza-foto" src="/res/pizzaH.jpg" alt="pizzaH"></div>
+                <div><img class="pizza-foto" src="../res/pizzaH.jpg" alt="pizzaH"></div>
                 <p>70ש"ח</p></a>
             </div>
 
             <div class="deal-conteiner">
                 <a href="popupPizza">
                 <h2>Large</h2>
-                <div><img class="pizza-foto" src="/res/pizzaL.jpg" alt="pizzaL"></div>
+                <div><img class="pizza-foto" src="../res/pizzaL.jpg" alt="pizzaL"></div>
                 <p>50ש"ח</p></a>
             </div>
 
             <div class="deal-conteiner">
                 <a href="popupPizza">
                 <h2>personal</h2>
-                <div><img class="pizza-foto" src="/res/isheet2.jpg" alt="pizzaP"></div>
+                <div><img class="pizza-foto" src="../res/isheet2.jpg" alt="pizzaP"></div>
                 <p>35ש"ח</p></a>
             </div>
         </div>
@@ -81,31 +74,31 @@
             <div class="deal-conteiner">
                 <a href="popupDrinks">
                 <h2>Cola</h2>
-                <div><img class="Drink-foto" src="/res/cola.jpg" alt="cola"></div>
+                <div><img class="Drink-foto" src="../res/cola.jpg" alt="cola"></div>
                 <p>12ש"ח</p></a>
             </div>
             <div class="deal-conteiner">
                 <a href="popupDrinks">
                 <h2>Sprite</h2>
-                <div><img class="Drink-foto" src="/res/sprite.jpg" alt="sprite"></div>
+                <div><img class="Drink-foto" src="../res/sprite.jpg" alt="sprite"></div>
                 <p>12ש"ח</p></a>
             </div>
             <div class="deal-conteiner">
                 <a href="popupDrinks">
                 <h2>Juce</h2>
-                <div><img class="Drink-foto" src="/res/juce.jpg" alt="juce"></div>
+                <div><img class="Drink-foto" src="../res/juce.jpg" alt="juce"></div>
                 <p>10ש"ח</p></a>
             </div>
             <div class="deal-conteiner">
                 <a href="popupDrinks">
                 <h2>Fanta</h2>
-                <div><img class="Drink-foto" src="/res/isheet2.jpg" alt="pizzaP"></div>
+                <div><img class="Drink-foto" src="../res/isheet2.jpg" alt="pizzaP"></div>
                 <p>12ש"ח</p></a>
             </div>
             <div class="deal-conteiner">
                 <a href="popupDrinks">
                 <h2>Nestea</h2>
-                <div><img class="Drink-foto" src="/res/nestea.jpg" alt="nestea"></div>
+                <div><img class="Drink-foto" src="../res/nestea.jpg" alt="nestea"></div>
                 <p>9ש"ח</p></a>
             </div>
         </div>
@@ -113,31 +106,31 @@
             <div class="deal-conteiner">
                 <a href="popupAdditives">
                 <h2>mushrooms</h2>
-                <div><img class="Additives-foto" src="/res/mushrooms.jpg" alt="mushrooms"></div>
+                <div><img class="Additives-foto" src="../res/mushrooms.jpg" alt="mushrooms"></div>
                 <p>5ש"ח</p></a>
             </div>
             <div class="deal-conteiner">
                 <a href="popupAdditives">
                 <h2>onion</h2>
-                <div><img class="Additives-foto" src="/res/onion.jpg" alt="onion"></div>
+                <div><img class="Additives-foto" src="../res/onion.jpg" alt="onion"></div>
                 <p>5ש"ח</p></a>
             </div>
             <div class="deal-conteiner">
                 <a href="popupAdditives">
                 <h2>tuna</h2>
-                <div><img class="Additives-foto" src="/res/tuna.jpg" alt="tuna"></div>
+                <div><img class="Additives-foto" src="../res/tuna.jpg" alt="tuna"></div>
                 <p>5ש"ח</p></a>
             </div>
             <div class="deal-conteiner">
                 <a href="popupAdditives">
                 <h2>bulgarit</h2>
-                <div><img class="Additives-foto" src="/res/bulgarit.jpg" alt="bulgarit"></div>
+                <div><img class="Additives-foto" src="../res/bulgarit.jpg" alt="bulgarit"></div>
                 <p>5ש"ח</p></a>
             </div>
             <div class="deal-conteiner">
                 <a href="popupAdditives">
                 <h2>tomatto</h2>
-                <div><img class="Additives-foto" src="/res/tomatto.jpg" alt="tomatto"></div>
+                <div><img class="Additives-foto" src="../res/tomatto.jpg" alt="tomatto"></div>
                 <p>5ש"ח</p></a>
             </div>
         </div>
@@ -145,41 +138,10 @@
 </div>
 
     <footer>
-        <h1>Our web-developers</h1>
-        <div class="developers">
-         <div class="dev">
-           <div><img class="foto-developer" src="/res/ya.jpg" alt=""></div>
-              <h3>Egor</h3>
-              <p>tel: 0533756636</p>
-              <p><a href="facebooke.com"><i class="fa-brands fa-facebook-f"></i></a>
-              <a href="https://www.instagram.com"><i class="fa-brands fa-instagram"></i></a>
-              <i class="fa-solid fa-envelope"></i></p>
-         </div>
-
-     
-            <div class="dev">
-              <div><img class="foto-developer" src="/res/ross.jpg" alt=""></div>
-              <h3>Ross</h3>
-              <p>tel: 0504483162</p>
-              <p><a href="facebooke.com"><i class="fa-brands fa-facebook-f"></i></a>
-               <a href="https://www.instagram.com"><i class="fa-brands fa-instagram"></i></a>
-               <i class="fa-solid fa-envelope"></i></p>
-           </div>
-
-           
-            <div class="dev">
-              <div><img class="foto-developer" src="https://media-exp2.licdn.com/dms/image/C4D03AQGEKtimVnnCAQ/profile-displayphoto-shrink_800_800/0/1656070867578?e=1661385600&v=beta&t=tW-yYjI3mVhAuFgBNrRkr1K1omyZw0jAnbpftoHScDk" alt=""></div>
-              <h3>Ben</h3>
-              <p>tel: 0507744439</p>
-              <p><a href="facebooke.com"><i class="fa-brands fa-facebook-f"></i></a>
-               <a href="https://www.instagram.com"><i class="fa-brands fa-instagram"></i></a>
-               <i class="fa-solid fa-envelope"></i></p>
-           </div>
-
-        </div>
+        <?php include './footer.php'; ?>
     </footer>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-    <script src="/htmlPizza/pizza.js"></script>
+    <script src="pizza.js"></script>
 </body>
 <div id="popupDeal" class="popup">
     <a href="#deals" class="popup_area"></a>
