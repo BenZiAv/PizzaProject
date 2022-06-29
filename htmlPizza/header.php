f3b0bb4ab8d41773280b9f4d94de03334663366f
BenZiAv <benavnilov@gmail.com>
2022-06-29 18:36:36 +0300

    .

--- htmlPizza/header.php
+++ htmlPizza/header.php
@@ -9,12 +9,11 @@
         <title>Pizza</title>
     </head>
 	  <div class="navbar">
-      <a href="../../htmlPizza/userForm.php"><i>Profile</i></a>
-      <a href="../../htmlPizza/order.php"><i>Order</i></a>
-      <a href="https://www.independent.co.uk/topic/pizza"><i>Pizza News</i></a>
-      <a href="#deals"><i>Hot deals</i></a>
-			<a href="../../htmlPizza/Register.php">Register</a>
-			<a href="LogIn.html">Log In</a>
-			<a href = "PHP/logout.php" tite = "Logout">Logout</a>
+        <a href="/htmlPizza/userForm.php"><i>Profile</i></a>
+        <a href="/htmlPizza/order.php"><i>Order</i></a>
+        <a href="https://www.independent.co.uk/topic/pizza"><i>Pizza News</i></a>
+        <a href="#deals"><i>Hot deals</i></a>
+		<a href="/htmlPizza/Register.php">Register</a>
+		<a href="LogIn.html">Log In</a>
 	  </div>
 
