18322fd3e375c48f94d25882219a6a8dea8365f4
BenZiAv <benavnilov@gmail.com>
2022-07-03 23:55:19 +0300

    .

--- htmlPizza/login.php
+++ htmlPizza/login.php
@@ -29,7 +29,7 @@ $phonenumber = mysqli_real_escape_string($con,$phonenumber);
 
 //Checking is user existing in the database or not
 $query = "SELECT * FROM `users` WHERE mail='$mail' and password='".md5($password)."'";
-$result = mysqli_query($con,$query) or die(mysqli_error());
+$result = mysqli_query($con,$query) or die(mysqli_error ($mysqli));
 $rows = mysqli_num_rows($result);
 if($rows==1){
 $_SESSION['mail'] = $mail;
